70123588438e117d5e67891530054143a6a46959
Alexandru Burduja <burduja_alex14@yahoo.com>
2022-11-08 13:00:07 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { Header } from './Header.js';
 import { Image } from './Image';
@@ -9,10 +10,60 @@ function App() {
       <Header></Header>
       <Image></Image>
       <ImageCards></ImageCards>
+      <ProductListComponent></ProductListComponent>
     </main>
   )
 }
 
 export default App;
 
+function ProductListComponent() {
+  const url = "http://localhost:3001"
+  const endpoint = "/products"
+  const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    fetch(url + endpoint)
+    .then((response) => response.json())
+    .then((moviesFromServer) => setMovies(moviesFromServer) )
+  }, [])
+
+  return (
+    <section>
+      <header>Products</header>
+
+      <ul>
+      {
+        movies.map((movie) => {
+          return(
+            <ProductCardComponent 
+            Title={movie.title}
+            Kg={movie.kg}
+            Currency = {movie.currency}
+            Price={movie.price}
+            Image={movie.image}>
+            </ProductCardComponent>
+          )
+        })
+      }
+      </ul>
+    </section>
+  )
+}
+
+function ProductCardComponent(props) {
+  const { Title, Kg, Price, Currency, Image } = props
+
+  return (
+    <li>
+      <article>
+        <h2>{Title}</h2>
+
+        <p>{Kg}</p>
+        <span>{Price} {Currency}</span>
+
+        <img src={Image} alt="productImage"/>
+      </article>
+    </li>
+  )
+}
--- src/ImageCards.js
+++ src/ImageCards.js
@@ -12,25 +12,25 @@ export function ImageCards() {
     <div className='card card1'>
             <FontAwesomeIcon icon={ faHouse } className='fontawesome'/>
             <p>Home</p>
-            <p><LoremIpsum avgWordsPerSentence={0.2} /></p>
+            <LoremIpsum avgWordsPerSentence={0.2} className='lorem' />
     </div>
 
     <div className='card card2'>
             <FontAwesomeIcon icon={ faUsers } className='fontawesome'/>
             <p>About Us</p>
-            <p><LoremIpsum avgWordsPerSentence={0.2} /></p>
+            <LoremIpsum avgWordsPerSentence={0.2} className='lorem' />
     </div>
 
     <div className='card card3'>
             <FontAwesomeIcon icon={ faThumbsUp } className='fontawesome' />
             <p>Guarantee</p>
-            <p><LoremIpsum avgWordsPerSentence={0.2} /></p>
+            <LoremIpsum avgWordsPerSentence={0.2} className='lorem' />
     </div>
 
     <div className='card card4'>
             <FontAwesomeIcon icon={ faEnvelope } className='fontawesome'/>
             <p>Get In Touch</p>
-            <p><LoremIpsum avgWordsPerSentence={0.2} /></p>
+            <LoremIpsum avgWordsPerSentence={0.2} className='lorem' />
     </div>
   </section>
   )

6aaa3aebd2e94b553ef46d79b59b591c3eaf2ef6
Alex Burduja <burduja_alex@yahoo.com>
2022-11-10 14:06:20 +0200

    ...

--- src/ProductCardComponent.js
+++ src/ProductCardComponent.js
@@ -6,7 +6,7 @@ export function ProductCardComponent(props) {
         <h2>{Title}</h2>
         <img src={Image} alt="productImage" />
 
-        <button>Buy!</button>
+        <button>Add to cart</button>
         <p>{Kg}</p>
         <span>{Price} {Currency}</span>
     </div>

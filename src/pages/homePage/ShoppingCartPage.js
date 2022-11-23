09281735ce663c83d0aa90d717d66b65acc05543
Burduja Alexandru <burduja_alex14@yahoo.com>
2022-11-23 21:40:40 +0200

    ...

--- src/pages/homePage/ShoppingCartPage.js
+++ src/pages/homePage/ShoppingCartPage.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import { useEffect, useState } from 'react';
 import "./ShoppingCartPage.css"
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 export function ShoppingCartPage() {
 
@@ -15,16 +17,24 @@ export function ShoppingCartPage() {
 
           setProducts(cart.products)
       })}, [] );
-    
+
+      let productQuantity = products.map(function (product){
+        return product.productPrice * product.quantity
+      })
+
+      
+      console.log(productQuantity)
 
 
   return (
     <section className='wrapper'>
+      <section className='cartPageLeftSection'>
       <h1>1. REVIEW YOUR ORDER </h1>
       <h3>Please check that you have the right quantity of every single item to avoid confusions at checkout, Thanks!</h3>
+
       {products.map((item) => {
         return (
-          <section className='cartPageLeftSection'>
+          <>
             <table className='desktopTable'>
               <tr className='firstRow'>
                 <th>Image</th>
@@ -96,31 +106,38 @@ export function ShoppingCartPage() {
                   </td>
               </tr>
             </table>
-          </section>
+            </>
       )
-})}
-    <div className='deliveryAddress'>
-      <div className='deliveryAddress_header'>
+    })}
+    </section>
+    <section className='deliveryAddress'>
+    <div className='deliveryAddress_header'>
         <h1>2. DELIVERY ADDRESS</h1>
         <h3>Please check that all the informations are valid and correct, Thanks!</h3>
         <h5>All fields required.</h5>
       </div>
 
       <div className="deliveryAddress_inputs">
-
         <div className='deliveryAddress_inputs__input' >
-          <input type="email" required="required" ></input>
+          <input type="text" required="required" ></input>
           <span>Email Address</span>
         </div>
 
-        <div className='deliveryAddress_inputs__input' >
-          <input type="text" required="required"></input>
-          <span>First name</span>
+        <div className='deliveryAddress_inputs__input input2' >
+          <div>
+            <input type="text" required="required"></input>
+            <span>First name</span>
+          </div>
+
+          <div className='lastNameInput'>
+            <input type="text" required="required"></input>
+            <span>Last name</span>
+          </div>
         </div>
         
-        <div className='deliveryAddress_inputs__input' >
-          <input type="text" required="required"></input>
-          <span>Last name</span>
+        <div className='deliveryAddress_inputs__input company'>
+          <input type="text" required></input>
+          <span>Company (optional)</span>
         </div>
 
         <div className='deliveryAddress_inputs__input' >
@@ -133,18 +150,48 @@ export function ShoppingCartPage() {
           <span>Delivery Address</span>
         </div>
 
-        <div className='deliveryAddress_inputs__input' >
-          <input type="text" required="required"></input>
-          <span>Country</span>
-        </div>
 
-        <div className='deliveryAddress_inputs__input' >
-          <input type="text" required="required"></input>
-          <span>Town</span>
+        <div className='deliveryAddress_inputs__input towninput' >
+
+          <div className='lastNameInput'>
+            <input type="text" required="required"></input>
+            <span>Town</span>
+          </div>
+
+          <div className='lastNameInput'>
+            <input type="text" required="required"></input>
+            <span>Country</span>
+          </div>
+
+          <div className='lastNameInput'>
+            <input type="text" required="required"></input>
+            <span>Zip Code</span>
+          </div>
+
         </div>
 
       </div>
-    </div>
+
+        <section className='checkoutTab'>
+          <div className='checkoutTab_header'>
+            <h1>3. CHECKOUT</h1>
+            <h3>Final process!</h3>
+          </div>
+
+          {products.map((item) => {
+              return (
+                <>
+                  <img src={item.productImage} width="50px"></img>
+                  <p>{item.productTitle}</p>
+                  <p>{item.quantity}</p>
+                  <p>{item.quantity * item.productPrice}</p>
+                </>
+              )
+          })}
+        </section>
+
+    </section>
+
     </section>
   )
   }

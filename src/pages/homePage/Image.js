3705f717d61227b6ae6d9710f88123cb1b603080
Alex Burduja <burduja_alex@yahoo.com>
2023-02-05 23:40:53 +0200

    ..

--- src/pages/homePage/Image.js
+++ src/pages/homePage/Image.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import warehousePhoto from "../../publicResources/warehouse.webp"
+import warehousePhotoBig from "../../publicResources/warehousePngBig.webp"
+import warehousePhotoMedium from "../../publicResources/warehousePngMedium.webp"
+import warehousePhotoSmall from "../../publicResources/warehousePngSmall.webp"
 import './Image.css'
 
 export function Image() {
+
+
   return (
     <section className='imageSection'>
-      <div>
-        <img srcSet="" alt="backgroundPhoto"/>
+        <div>
+        <img 
+        src={warehousePhotoBig}
+        srcSet={ `${warehousePhotoSmall} 500w , ${warehousePhotoMedium} 900w ,${warehousePhotoBig} 1280w` }
+        sizes={"(max-width:500px) 500px,(max-width:900) 900px, 1280px"}
+        alt="backgroundPhoto"/>
       </div>
+     
 
     <div className='imageInfo'>
       <p>Mabis <span className='woodColor'>Wood</span><span className='ekoColor'> Eko</span>,</p>

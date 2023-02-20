7c6c6659fbf72f4318eb8a34fe7a94de0a35592c
Alexandru Burduja <burduja_alex14@yahoo.com>
2023-02-20 21:15:08 +0200

    ...

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { Document, Page } from 'react-pdf'
 import pdf from './publicResources/Privacy Policy Mabis Wood Eko.pdf'
+import './PrivacyPolicyCss.css'
 
 function PrivacyPolicy() {
   
@@ -13,14 +14,12 @@ function PrivacyPolicy() {
   }
 
     return (
-    <div>
-    <Document file={'./publicResources/Privacy Policy Mabis Wood Eko.pdf'} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-    </Document>
-    <p>
-        Page {pageNumber} of {numPages}
-  </p>
-  </div>
+        // <div className='divPrivacy'>
+        //     <iframe src={pdf} title="privacyPdf" width='100%' height='100%' className='iframePrivacy'/>
+        // </div>
+        <Document file={pdf} >
+            <Page pageNumber={1} />
+        </Document>
   )
 }
 

f40d8d39b08ad43d63adf9cd9a0922ba3fec616e
Alex Burduja <burduja_alex@yahoo.com>
2023-02-20 23:29:03 +0200

    ...

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -1,26 +1,31 @@
-import React from 'react'
 import { useState } from 'react';
-import { Document, Page } from 'react-pdf'
-import pdf from './publicResources/Privacy Policy Mabis Wood Eko.pdf'
-import './PrivacyPolicyCss.css'
+import { Document, Page, pdfjs } from 'react-pdf';
+import './PrivacyPolicyCss.css';
 
 function PrivacyPolicy() {
+  const url = './publicResources/Cookie Policy Mabis Wood Eko.pdf';
   
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
 
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
 
-    return (
-        // <div className='divPrivacy'>
-        //     <iframe src={pdf} title="privacyPdf" width='100%' height='100%' className='iframePrivacy'/>
-        // </div>
-        <Document file={pdf} >
-            <Page pageNumber={1} />
-        </Document>
-  )
+  return (
+    <>
+    <Document file={'./publicResources/Cookiee.pdf'} onLoadSuccess={onDocumentLoadSuccess}>
+      <Page pageNumber={1} />
+      <Page pageNumber={2} />
+    </Document>
+
+    <p>
+    Page {pageNumber} of {numPages}
+    </p>
+    </>
+  );
 }
 
-export default PrivacyPolicy
+export default PrivacyPolicy;
